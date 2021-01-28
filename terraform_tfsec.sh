25456b2109e4bb66e2889cd6907b9f8b419a4aa9
Pawel Fiuto <pawel.fiuto@claranet.uk>
2021-01-28 13:37:47 +0000

    echo args

--- terraform_tfsec.sh
+++ terraform_tfsec.sh
@@ -22,6 +22,7 @@ tfsec_() {
   for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
     path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
     pushd "$path_uniq" > /dev/null
+    echo $ARGS
     tfsec $ARGS
     popd > /dev/null
   done
